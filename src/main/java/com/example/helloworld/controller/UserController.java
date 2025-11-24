120a67f71a6d597ce12ddaa8a79b71c07d0304cf
Rajan <109027109+rajan0031@users.noreply.github.com>
2025-11-24 09:56:14 +0000

    added

--- src/main/java/com/example/helloworld/controller/UserController.java
+++ src/main/java/com/example/helloworld/controller/UserController.java
@@ -1,12 +1,28 @@
 package com.example.helloworld.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.helloworld.model.User;
+import com.example.helloworld.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hey is the calll from the backend";
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
     }
 }
